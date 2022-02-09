0d6414b4a2b3f02d779fccedb530a919247edb8f
webdeveloper510 <developerweb6@gmail.com>
2022-02-10 04:44:45 +0530

    changes

--- RestApi/settings.py
+++ RestApi/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = 'RestApi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'RestDb',
+        'NAME': 'rdb',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
