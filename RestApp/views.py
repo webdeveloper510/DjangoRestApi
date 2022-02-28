63f2d558c4edd714ff7e5ac3e868e56f17379ba1
webdeveloper510 <developerweb6@gmail.com>
2022-02-28 22:23:44 +0530

    resolved issue in show masterlist api of showing double data

--- RestApp/views.py
+++ RestApp/views.py
@@ -420,12 +420,9 @@ def GETMasterListRequest(request,pk):
     Masterrecord  = []
     Pages = 10
     data_dict = MasterList.objects.filter(projectId=pk).values().order_by("id")[:10]
-    # data_dict = MasterList.objects.filter(projectId=pk).values()
     data_count = MasterList.objects.filter(projectId=pk).values().count()
-    # print(data_dict.count)
     PagesCount = data_count/10
     for masterlistdata in data_dict:
-        Masterrecord.append(masterlistdata)
         Teamsquery = Teams.objects.filter(id=masterlistdata['TeamName_id']).values('id','TeamNames')
         masterlistdata['TeamName_id'] = Teamsquery[0].copy()
         masterlistdata['Original_Owner_id'] = Teamsquery[0].copy()
@@ -433,9 +430,8 @@ def GETMasterListRequest(request,pk):
         ProjectQuery = Project.objects.filter(id=masterlistdata['projectId_id']).values('id','project_name')
         masterlistdata['projectId_id'] = ProjectQuery[0].copy()
         Masterrecord.append(masterlistdata)
-        MasterListt  = Masterrecord
         print(Masterrecord)
-    return Response({'data': MasterListt,'PagesCount':PagesCount}, status=status.HTTP_200_OK)
+    return Response({'data': Masterrecord,'PagesCount':PagesCount}, status=status.HTTP_200_OK)
 
 
 @ api_view(['GET'])
