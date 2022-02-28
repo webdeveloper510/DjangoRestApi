9876c2c213d4cd1141e36efe14789fa1bd3d2aac
root <root@ip-172-31-35-209.ap-southeast-2.compute.internal>
2022-02-28 16:54:46 +0000

    resolved issue of shoing double records in showmasterlist api

--- RestApp/views.py
+++ RestApp/views.py
@@ -414,7 +414,7 @@ def LadderRequest(request):
     return Response(LadderList, status=status.HTTP_200_OK)
 
 
-@ api_view(['POST'])
+@ api_view(['GET'])
 @ permission_classes([AllowAny, ])
 def GETMasterListRequest(request,pk):
     Masterrecord  = []
