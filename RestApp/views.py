077070b6c8b237a56d43942f33b56cd0d054ad80
webdeveloper510 <developerweb6@gmail.com>
2022-07-01 04:45:42 +0530

    resolved issues in visualisations api and priority pick

--- RestApp/views.py
+++ RestApp/views.py
@@ -3952,6 +3952,8 @@ def add_trade_v3(request, pk):
     current_date = date.today()
     v_current_year = current_date.year
     for team2pickout in team2_trades_picks:
+        team2pickout.sort_index(inplace=True)
+
         # Changing the previous owner name
 
         masterlist['Previous_Owner'].mask(
@@ -3964,6 +3966,7 @@ def add_trade_v3(request, pk):
         # Loop for each pick that team 1 is trading out to team 2:
 
     for team1pickout in team1_trades_picks:
+        team1pickout.sort_index(inplace=True)
         # Changing the previous owner name
 
         masterlist['Previous_Owner'].mask(masterlist['Display_Name_Detailed'] == team1pickout, masterlist['Current_Owner'], inplace=True)
