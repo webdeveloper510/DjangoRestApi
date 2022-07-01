d97e80479c5ff10256fa6ff56e03a5af9e382a26
webdeveloper510 <developerweb6@gmail.com>
2022-07-01 06:03:08 +0530

    resolved issues in visualisations api and priority pick

--- RestApp/views.py
+++ RestApp/views.py
@@ -3952,7 +3952,6 @@ def add_trade_v3(request, pk):
     current_date = date.today()
     v_current_year = current_date.year
     for team2pickout in team2_trades_picks:
-        print(team2pickout)
 
         # Changing the previous owner name
 
@@ -4027,8 +4026,7 @@ def add_trade_v3(request, pk):
     trade_dict = {}
     trade_dict[Team1_name] = team1_trades_picks 
     trade_dict[Team2_name] = team2_trades_picks
-    print(team2_trades_pick_names)
-    exit()
+ 
     trade_dict_full = {Team1_name: [team1_trades_players,team1_trades_picks,team1_trades_pick_names] , Team2_name: [team2_trades_players,team2_trades_picks,team2_trades_pick_names]}
      #Creating a written description
     trade_description = str(Team1_name) + ' traded ' +  str(team1_out) + ' & ' + str(Team2_name) + ' traded ' + str(team2_out)
